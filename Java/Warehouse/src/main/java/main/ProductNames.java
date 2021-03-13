aa503416e7a5bbdbd2ee97e3cb3434d9f7a9973c
meAlexander <52578564+meAlexander@users.noreply.github.com>
2021-03-13 23:58:29 +0200

    Delete Java directory

--- Java/Warehouse/src/main/java/main/ProductNames.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public enum ProductNames {
-    BANANA, ORANGE, APPLE,
-    POTATO, EGGPLANT, CUCUMBER,
-    PORK, BEEF, CHICKEN
-}
--- Java/Warehouse/src/main/java/main/ProductType.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public enum ProductType {
-    FRUITS, VEGETABLES, MEATS
-}
